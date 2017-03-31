221a2c99a84231a7d4a357577da123a272ef7cc7
CONSTANTIN <benoitc@equilibregames.com>
2017-03-31 13:41:25 +0200

    -ADD : SmartLocalization

---
+++ IntegrationDesk/IntegrationDesk/Assets/StandardAssets/SmartLocalization/Editor/TextManagerEditor.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using SmartLocalization.Editor;
+using SmartLocalization;
+
+using EquilibreGames;
+
+[CustomEditor(typeof(TextManager))]
+public class TextManagerEditor : Editor {
+
+    private List<string> selectedKeys = new List<string>();
+
+     public override void OnInspectorGUI()
+    {
+       // base.OnInspectorGUI();
+        TextManager textObject = ((TextManager)target);
+        int cpt = 0;
+
+        selectedKeys.Clear();
+
+
+        if (textObject != null)
+        {
+            int value = (int)Mathf.Clamp(EditorGUILayout.IntField("SystemLanguage : " , textObject.systemLanguages.Count),0,Mathf.Infinity);
+
+            if (value > textObject.systemLanguages.Count)
+            {
+                for (int i = value; textObject.systemLanguages.Count < value; i++)
+                    textObject.systemLanguages.Add(new TextManager.LanguageConversionData());
+            }
+            else if (value < textObject.systemLanguages.Count)
+            {
+                for (int i = value; textObject.systemLanguages.Count > value; i--)
+                    textObject.systemLanguages.RemoveAt(textObject.systemLanguages.Count - 1);
+            }
+
+
+            foreach (TextManager.LanguageConversionData i in textObject.systemLanguages)
+            {
+                i.systemLanguage = (SystemLanguage)EditorGUILayout.EnumPopup("SystemLanguage : ", i.systemLanguage);
+                i.smartLocalizationKey = EditorGUILayout.TextField("SmartLocalisaiton Key : ", i.smartLocalizationKey);
+                GUILayout.Space(5);
+            }
+
+
+            GUILayout.Space(20);
+
+
+
+            value = (int)Mathf.Clamp(EditorGUILayout.IntField("SmartText : ", textObject.smartTextData.Count), 0, Mathf.Infinity);
+
+            if (value > textObject.smartTextData.Count)
+            {
+                for (int i = value; textObject.smartTextData.Count < value; i++)
+                    textObject.smartTextData.Add(new TextManager.SmartTextData());
+            }
+            else if (value < textObject.smartTextData.Count)
+            {
+                for (int i = value; textObject.smartTextData.Count > value; i--)
+                textObject.smartTextData.RemoveAt(textObject.smartTextData.Count - 1);
+            }
+
+
+            foreach (TextManager.SmartTextData i in textObject.smartTextData)
+            {
+                selectedKeys.Add(i.localizedKey);
+            }
+
+            foreach (TextManager.SmartTextData i in textObject.smartTextData)
+            {
+                i.text = (Text)EditorGUILayout.ObjectField(i.text, typeof(Text), true);
+                i.localizedKey = EditorGUILayout.TextField(i.localizedKey);
+
+                selectedKeys[cpt] = LocalizedKeySelector.SelectKeyGUI(selectedKeys[cpt], true, LocalizedObjectType.STRING);
+
+                if (!Application.isPlaying && GUILayout.Button("Use Key", GUILayout.Width(70)))
+                {
+                    i.localizedKey = selectedKeys[cpt];
+                }
+                GUILayout.Space(10);
+                cpt++;
+            }
+        }
+    }
+}
---
+++ IntegrationDesk/IntegrationDesk/Assets/StandardAssets/SmartLocalization/TextManager.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using SmartLocalization.Editor;
+using SmartLocalization;
+
+
+namespace EquilibreGames
+{
+    public class TextManager : Singleton<TextManager>
+    {
+        public enum TEXTMANAGEREVENT { ONLANGUAGECHANGED, }
+
+        [System.Serializable]
+        public class SmartTextData
+        {
+            public Text text;
+            public string localizedKey = "INSERT_KEY_HERE";
+        }
+
+        public List<SmartTextData> smartTextData = new List<SmartTextData>();
+
+        [System.Serializable]
+        public class LanguageConversionData
+        {
+            public SystemLanguage systemLanguage;
+            public string smartLocalizationKey;
+        }
+
+
+        public string currentLanguage;
+
+        public List<LanguageConversionData> systemLanguages;
+
+
+        void Awake()
+        {
+            SmartLocalization.LanguageManager languageManager = SmartLocalization.LanguageManager.Instance;
+
+            //Subscribe to the change language event
+            languageManager.OnChangeLanguage += OnChangeLanguage;
+
+            foreach (LanguageConversionData i in systemLanguages)
+            {
+                if (Application.systemLanguage == i.systemLanguage)
+                {
+                    SmartLocalization.LanguageManager.Instance.ChangeLanguage(i.smartLocalizationKey);
+                    return;
+                }
+            }
+
+
+
+            //Run the method one first time if no language correspondance found.
+            ChangeLanguage(SmartLocalization.LanguageManager.Instance.defaultLanguage);
+        }
+
+
+        void OnDestroy()
+        {
+            if (SmartLocalization.LanguageManager.HasInstance)
+            {
+                SmartLocalization.LanguageManager.Instance.OnChangeLanguage -= OnChangeLanguage;
+            }
+        }
+
+
+        public void ChangeLanguage(SystemLanguage systemLanguage)
+        {
+            foreach (LanguageConversionData i in systemLanguages)
+            {
+                if (Application.systemLanguage == i.systemLanguage)
+                {
+                    SmartLocalization.LanguageManager.Instance.ChangeLanguage(i.smartLocalizationKey);
+                    currentLanguage = i.smartLocalizationKey;
+                    return;
+                }
+            }
+        }
+
+        public void ChangeLanguage(string smartLocalizationKeyLanguage)
+        {
+            SmartLocalization.LanguageManager.Instance.ChangeLanguage(smartLocalizationKeyLanguage);
+            currentLanguage = smartLocalizationKeyLanguage;
+        }
+
+
+        void OnChangeLanguage(LanguageManager languageManager)
+        {
+#if EQUILIBRE_GAMES_DEBUG
+            Debug.Log("Language changed");
+#endif
+            foreach (SmartTextData i in smartTextData)
+            {
+                i.text.text = languageManager.GetTextValue(i.localizedKey);
+            }
+#if EVENTHANDLER
+            EventHandler.TriggerEvent(TEXTMANAGEREVENT.ONLANGUAGECHANGED);
+#endif
+        }
+
+    }
+}
