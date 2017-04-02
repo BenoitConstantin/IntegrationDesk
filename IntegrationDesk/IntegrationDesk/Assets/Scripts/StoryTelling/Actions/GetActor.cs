5c00bf0bd3893c91d5a3646a584d81a553e7eb60
CONSTANTIN <benoitc@equilibregames.com>
2017-04-02 21:58:30 +0200

    -MOD : le mapping pour les acteurs des dialogues.

--- IntegrationDesk/IntegrationDesk/Assets/Scripts/StoryTelling/Actions/GetActor.cs
+++ IntegrationDesk/IntegrationDesk/Assets/Scripts/StoryTelling/Actions/GetActor.cs
@@ -7,13 +7,11 @@ using NodeCanvas.DialogueTrees;
 public class GetActor : ActionTask {
 
     public string actorName;
-    public BBParameter<DialogueActor> dialogueActor;
     public DialogueTreeController dialogueTreeController;
 
     protected override void OnExecute()
     {
-        dialogueActor.value = StoryTelling.Instance.GetDialogueActor(actorName);
-        dialogueTreeController.behaviour.actorParameters.Add(new DialogueTree.ActorParameter(actorName, dialogueActor.value));
+        dialogueTreeController.behaviour.SetActorReference(actorName, StoryTelling.Instance.GetDialogueActor(actorName));
         EndAction();
     }
 
