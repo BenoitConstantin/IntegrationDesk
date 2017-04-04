0c983cef88ca29cb59db43034b35a80b9546749d
CONSTANTIN <benoitc@equilibregames.com>
2017-04-04 11:22:32 +0200

    -ADD : Node pour parler plusieurs fois à la même personne.

--- IntegrationDesk/IntegrationDesk/Assets/Scripts/StoryTelling/StoryScene.cs
+++ IntegrationDesk/IntegrationDesk/Assets/Scripts/StoryTelling/StoryScene.cs
@@ -2,11 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 using NodeCanvas.BehaviourTrees;
+using System;
+using NodeCanvas.DialogueTrees;
 
 public class StoryScene : MonoBehaviour {
+
+    public static Action<string> OnSpeakWithActor;
 
     [SerializeField]
     BehaviourTreeOwner behaviourTree;
 
+    [SerializeField]
+    DialogueTreeController dialogueTreeController;
 
+    void Awake()
+    {
+        OnSpeakWithActor += SpeakWithActor;
+    }
+
+    void OnDestroy()
+    {
+        OnSpeakWithActor -= SpeakWithActor;
+    }
+
+
+    public void SpeakWithActor(string actorName)
+    {
+        behaviourTree.SendEvent<string>("Speak", actorName);
+    }
 }
