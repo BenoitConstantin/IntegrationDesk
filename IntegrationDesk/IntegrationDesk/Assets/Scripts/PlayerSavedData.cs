d97d57e0063f23b98886ab959056d208df608c77
CONSTANTIN <benoitc@equilibregames.com>
2017-04-18 13:36:49 +0200

    -ADD : PlayerSavedData pour sauvegarder le score d'intégration du joueur.

---
+++ IntegrationDesk/IntegrationDesk/Assets/Scripts/PlayerSavedData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EquilibreGames;
+
+public class PlayerSavedData : SavedData {
+
+    public int integrationScore = 0;
+
+}
